47ca8a7c4b3a9d5b2861cbef9eafdc5db3877736
Hira <hira@Hiras-MacBook-Pro.local>
2024-06-10 22:48:40 +0600

    added rate limit

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-
+const rateLimit = require('express-rate-limit');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Enable CORS for all routes
 app.use(cors());
 
-// Serve the DASH manifest file
-app.get('/video.mpd', (req, res) => {
+// Rate limit for serving video files
+const videoRateLimitSegments = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 600, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests, please try again later.'
+});
+
+// Rate limit for serving video files
+const videoRateLimitMpd = rateLimit({
+    windowMs: 1 * 60 * 1000, // 10 minutes
+    max: 6, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests, please try again later.'
+});
+
+// Serve the DASH manifest file with rate limiting
+app.get('/video.mpd',videoRateLimitMpd, (req, res) => {
     console.log("called for mpd");
     const filePath = path.join(__dirname, 'video', 'video.mpd');
     res.sendFile(filePath);
 });
 
-// Serve the DASH segment files
-app.get('/video/:segment', (req, res) => {
+// Serve the DASH segment files with rate limiting
+app.get('/video/:segment', videoRateLimitSegments, (req, res) => {
     console.log("called segment");
     const segment = req.params.segment;
     const filePath = path.join(__dirname, 'video', segment);
--- command.sh
+++ command.sh
@@ -1,2 +1,15 @@
 ffmpeg -i video.mp4 -map 0 -b:v 2400k -s:v 1920x1080 -c:v libx264 -an -f dash video/video.mpd
  
+
+ ffmpeg -i video.mp4 \
+-map 0 \
+-b:v 2400k \
+-s:v 1920x1080 \
+-c:v libx264 \
+-an \
+-f dash \
+-seg_duration 4 \          # Sets segment duration to 4 seconds
+-video_chunk_duration 4 \  # Sets the chunk duration for video segments
+-init_seg_name init-\$RepresentationID\$.m4s \
+-media_seg_name chunk-\$RepresentationID\$-\$Number%05d\$.m4s \
+video/video.mpd
